a9e461aa12ff82795fddc265f43f940e089d2e39
Koras02 <wjdgh0727@gmail.com>
2022-03-01 17:51:29 +0900

    테스트

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 
+// 테스트
+
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
